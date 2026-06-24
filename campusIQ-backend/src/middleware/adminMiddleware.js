3b173996cbd2b02e9276247f6460bee7b550bf59
Harsh <harshsingh3776@gmail.com>
2026-06-24 10:37:13 +0530

    corrected directory

--- campusIQ-backend/src/middleware/adminMiddleware.js
+++
@@ -1,7 +0,0 @@
-export const adminOnly = (req, res, next) => {
-  if (req.admin && req.admin.role === 'admin') {
-    next();
-  } else {
-    res.status(403).json({ message: 'Admin access only' });
-  }
-};
